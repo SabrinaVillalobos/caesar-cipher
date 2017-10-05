1c7d45a95989d82e19774108b4f952487c098af3
Wafflo <wafflish@gmail.com>
2017-10-05 00:58:49 -0300

    actualización, sin tanto sueño

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Si el usuario ingresa una palabra, se llama la función cipher/decipher.
 La función cipher/decipher crea un string vacio, que será la variable en la que se almacenaran los nuevos carácteres. Se inicia un bucle for que pasa por cada letra y newWord.charCodeAt(i) devuelve el unicode correspondiente al index del string.
 Por medio de un if ingresó el rango de carácteres ASCII correspondientes a las letras mayúsculas, el resultado del bucle for más String.fromCharCode que convierte un numero Unicode a una letra y la formula del Código César dan como resultado la salida para un string con letras mayúsculas.
 Luego con el rango que corresponde a las letras minúsculas se aplica la formula y el resultado corresponde a la salida de un string con letras minúsculas.
-![Diagrama de Flujo](https://i.imgur.com/Ixh0W5j.png?raw=true
+
+![Diagrama de Flujo](https://i.imgur.com/dc1UFSS.jpg?raw=true)
--- app.js
+++ app.js
@@ -3,82 +3,70 @@ function caesarCipher() {
         var answer = prompt("Indique el número de lo que desea hacer: 1) Cifrar - 2) Decifar");
         if (answer !== "") {
             if (answer == "1") {
-                validateCipher();
+                cipher();
             } else if (answer == "2") {
-                validateDecipher();
+                decipher();
             } else {
                 alert("Ingrese una opción válida");
             }
         }
-    } while (answer === "" || (answer != "1" && answer != "2"));
+    } while (answer === "" || (answer != "1" && answer != "2")); //loop hasta que ingrese 1 o 2
 }
 
-var newWord = "";
-
-function validateCipher() {
-    do {
-        var newWord = prompt("Ingrese una palabra");
-        var alphaExp = /^[a-zA-Z]+$/;
-        if (newWord !== "") {
-            if (newWord.match(alphaExp)) {
-                cipher();
-            } else {
-                alert("Debe ingresar una palabra");
+do {
+    var word = prompt("Ingrese una palabra");
+    var alphaExp = /^[a-zA-Z]+$/; //expresión regular para buscar letras de la a-Z/A-Z
+    if (word !== "") {
+        if (this.word.match(alphaExp) || this.answer == "1") { //a prueba y error aprendí
+            this.cipher; //que no funcionaba sin el this por estar fuera del scope
+            if (this.word.match(alphaExp) || this.answer == "2") {
+                this.decipher;
             }
+        } else {
+            alert("Debe ingresar una palabra, sin espacios ni números");
         }
-    } while (newWord === "" || !/^[a-zA-Z]+$/.test(newWord));
-}
+    }
+} while (word === "" || !/^[a-zA-Z]+$/.test(word)); 
+
 
 function cipher() {
-    var output = ""; //variable de salida
-    for (var i = 0; i < newWord.length; i++) { //bucle para ir por cada letra
-        var character = newWord.charCodeAt(i); // variable para encontrar el N° de la letra
-        if (character >= 65 && character <= 90) { //si son mayusculas
-            output = output + String.fromCharCode((character - 65 + 33) % 26 + 65);
+
+    var output = ""; 
+
+    for (var i = 0; i < this.word.length; i++) { 
+        var character = this.word.charCodeAt(i); // da el N° unicode de la letra
+        var caesarCiphLow = ((character - 65 + 33) % 26 + 65); //formula que dio Michelle
+        var caesarCiphUpp = ((character - 97 + 33) % 26 + 97);//modificada para mayus y minus
+        if (character >= 65 && character <= 90) { 
+            output = output + String.fromCharCode(caesarCiphLow);
             //aplica Codigo Cesar (String.fromCharCode) devuelve una cadena a partir de n° unicode
-        } else if (97 <= character && character <= 122) { //si son minusculas
-            output = output + String.fromCharCode((character - 97 + 33) % 26 + 97); //aplicar codigo cesar
-        } else output = output + output.charAt(i);
+        } else if (97 <= character && character <= 122) { //si son mayus
+            output = output + String.fromCharCode(caesarCiphUpp); //aplicar codigo cesar
+        }
     }
     return alert(output);
+    
 }
+ function decipher() {
 
-function validateDecipher() {
-    do {
-        var newWord = prompt("Ingrese una palabra");
-        var alphaExp = /^[a-zA-Z]+$/;
-        if (newWord !== "") {
-            if (newWord.match(alphaExp)) {
-                decipher();
-            } else {
-                alert("Debe ingresar una palabra");
+        var output = ""; 
+        for (var i = 0; i < this.word.length; i++) { 
+            var character = this.word.charCodeAt(i); 
+            var caesarCiphLow = ((character - 65 - 7 + 52) % 26 + 65);
+            var caesarCiphUpp = ((character - 97 - 7 + 52) % 26 + 97);
+            if (character >= 65 && character <= 90) { 
+                output = output + String.fromCharCode(caesarCiphLow);
+            } else if (97 <= character && character <= 122) { 
+                output = output + String.fromCharCode(caesarCiphUpp); 
             }
         }
-    } while (newWord === "" || !/^[a-zA-Z]+$/.test(newWord));
-
-
-    function decipher() {
-        var output = ""; //variable de salida
-        for (var i = 0; i < newWord.length; i++) { //bucle para ir por cada letra
-            var character = newWord.charCodeAt(i); // variable para encontrar el N° de la letra
-            if (character >= 65 && character <= 90) { //si son mayusculas
-                output = output + String.fromCharCode((character - 65 - 33 + 26) % 26 + 65);
-                //aplica Codigo Cesar (String.fromCharCode) devuelve una cadena a partir de n° unicode
-            } else if (97 <= character && character <= 122) { //si son minusculas
-                output = output + String.fromCharCode((character - 97 - 33 + 26) % 26 + 97); //aplicar codigo cesar
-            } else output = output + output.charAt(i);
-        }
         return alert(output);
-    }
 }
-
-
 caesarCipher();
 
 /* A=65, Z=90 ---> mayus
    a=97, z=122 ---> minus
-
-
-
-            // sumar el 26 dentro antes de aplicar modulo
-            //asi no da negativo
+   Tuve muchos problemas con las variables globales y locales, a prueba y error
+   pude hacer que compilara pero en realidad no me queda claro del todo.
+   String.fromCharCode = ej: "ABC" = String.fromCharCode(65,66,67)
+*/
